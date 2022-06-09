2dd9309ca9ad13b02c80c9eb518a252607dd64da
lsepu <55538804+lsepu@users.noreply.github.com>
2022-06-09 11:25:56 -0500

    ADD: get all bills and receipts use cases created

--- src/main/java/com/hardwareStore/hardwareStore/usecases/GetAllBillsUseCase.java
+++ src/main/java/com/hardwareStore/hardwareStore/usecases/GetAllBillsUseCase.java
@@ -1,4 +1,23 @@
 package com.hardwareStore.hardwareStore.usecases;
 
+import com.hardwareStore.hardwareStore.mapper.StoreMapper;
+import com.hardwareStore.hardwareStore.model.BillDTO;
+import com.hardwareStore.hardwareStore.repository.BillRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+
+@Service
+@AllArgsConstructor
 public class GetAllBillsUseCase {
+
+    private final BillRepository repository;
+    private final StoreMapper storeMapper;
+
+
+    public Flux<BillDTO> get(){
+        return repository.findAll().map(storeMapper::convertBillToBillDTO);
+    }
+
 }
--- src/main/java/com/hardwareStore/hardwareStore/usecases/GetAllReceiptsUseCase.java
+++ src/main/java/com/hardwareStore/hardwareStore/usecases/GetAllReceiptsUseCase.java
@@ -1,4 +1,22 @@
 package com.hardwareStore.hardwareStore.usecases;
 
+import com.hardwareStore.hardwareStore.mapper.StoreMapper;
+import com.hardwareStore.hardwareStore.model.ReceiptDTO;
+import com.hardwareStore.hardwareStore.repository.ReceiptRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+@Service
+@AllArgsConstructor
 public class GetAllReceiptsUseCase {
+
+    private final ReceiptRepository repository;
+    private final StoreMapper storeMapper;
+
+
+    public Flux<ReceiptDTO> get(){
+        return repository.findAll().map(storeMapper::convertReceiptToReceiptDTO);
+    }
+
 }
